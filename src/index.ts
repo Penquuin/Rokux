10ea6ffa4bee0a58ac68fe4c249b685fbd322bd3
StormArmy <64629679+ThatLou@users.noreply.github.com>
2021-07-29 19:03:29 +0800

    small patch #5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import { Action, AnyAction, ActionWithState, Reducer, ReadonlyMiddleware } from "rokux-types";
-import { CreateReducer, Store } from "rokux";
+import {
+	Action as action,
+	AnyAction as anyAction,
+	ActionWithState as actionWithState,
+	Reducer as reducer,
+	ReadonlyMiddleware as readonlyMiddleware,
+} from "rokux-types";
 
-declare namespace Rokux {
-	export { Action, AnyAction, ActionWithState, Reducer, ReadonlyMiddleware, CreateReducer, Store };
+import { CreateReducer as createReducer, Store as store } from "rokux";
+
+namespace Rokux {
+	export const CreateReducer: typeof createReducer = createReducer;
+	export const Store: typeof store = store;
+
+	export type Action = action;
+	export type AnyAction = anyAction;
+	export type ActionWithState<S, A extends Action> = actionWithState<S, A>;
+	export type ReducerM<S, A extends Action> = reducer<S, A>;
+	export type ReadonlyMiddleware<S, A extends Action> = readonlyMiddleware<S, A>;
 }
 
 export default Rokux;
